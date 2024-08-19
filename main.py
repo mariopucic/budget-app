097d67466ad34b2233db51e78b3aa10d5461c058
mariopucic <mpucic@student.unipu.hr>
2024-08-19 17:44:00 +0200

    Dodana ruta POST za kreiranje novog budzeta, kreiran budzet model pomocu Pydantic za validaciju ulazinih podataka.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -14,6 +16,12 @@ def get_db():
     finally:
         db.close()
 
+class Budget(BaseModel):
+    name: str
+    description: Optional[str]
+    amount: int = Field(gt=0)
+
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Budgets).all()
@@ -24,6 +32,21 @@ async def read_budget(budget_id: int, db: Session = Depends(get_db)):
     if budget_model is not None:
         return budget_model
     raise http_expection()
+
+@app.post("/")
+async def create_budget(budget: Budget, db: Session = Depends(get_db)):
+    budget_model = models.Budgets()
+    budget_model.name = budget.name
+    budget_model.description = budget.description
+    budget_model.amount = budget.amount
+
+    db.add(budget_model)
+    db.commit()
+
+    return {
+        'status': 201,
+        'transaction': 'Successful'
+    }
 
 def http_expection():
     return HTTPException(status_code=404, detail="Budget not found")
