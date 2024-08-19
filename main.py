8e989bdaab41ef0ceec7c618a6299b79fe6123ac
mariopucic <mpucic@student.unipu.hr>
2024-08-19 15:00:38 +0200

    Dodana ruta budget/budget_id za dohvacanje budzeta pream ID-u, implementirana HTTP 404 greska.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 import models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
-async def create_database():
-    return {"Database": "Created"}
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Budgets).all()
+
+@app.get("/budget/{budget_id}")
+async def read_budget(budget_id: int, db: Session = Depends(get_db)):
+    budget_model = db.query(models.Budgets).filter(models.Budgets.id == budget_id).first()
+    if budget_model is not None:
+        return budget_model
+    raise http_expection()
+
+def http_expection():
+    return HTTPException(status_code=404, detail="Budget not found")
