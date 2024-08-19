4ccf360907d6dd3a330a505896aad6cafe541978
mariopucic <mpucic@student.unipu.hr>
2024-08-19 22:11:10 +0200

    Dodana PUT ruta za azuriranje postojeceg budzeta.

--- main.py
+++ main.py
@@ -48,5 +48,24 @@ async def create_budget(budget: Budget, db: Session = Depends(get_db)):
         'transaction': 'Successful'
     }
 
+@app.put("/{budget_id}")
+async def update_budget(budget_id: int, budget: Budget, db: Session = Depends(get_db)):
+    budget_model = db.query(models.Budgets).filter(models.Budgets.id == budget_id).first()
+    
+    if budget_model is None:
+        raise http_expection()
+    
+    budget_model.name = budget.name
+    budget_model.description = budget.description
+    budget_model.amount = budget.amount
+
+    db.add(budget_model)
+    db.commit()
+
+    return {
+        'status': 200,
+        'transaction': 'Successful'
+    }
+
 def http_expection():
     return HTTPException(status_code=404, detail="Budget not found")
