c53367388defe8764d7f2f71d91b69d72033b7a2
mariopucic <mpucic@student.unipu.hr>
2024-08-20 16:21:55 +0200

    Dodana klasa Users u za korisnicke podatke, poboljsana struktura baze podataka dodavanjem novih polja za korisnike.

--- models.py
+++ models.py
@@ -1,5 +1,19 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+class Users(Base):
+    __tablename__= "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    budgets = relationship("Budgets", back_populates="owner")
+
 
 class Budgets(Base):
     __tablename__ = "budgets"
@@ -7,3 +21,6 @@ class Budgets(Base):
     name = Column(String, index=True)
     description = Column(String, index=True)
     amount = Column(Float)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="budgets")
